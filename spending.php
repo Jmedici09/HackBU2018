0a65943138821157d6e8f98d5538e95089cbce1c
Jmedici09 <Jmedici09@gmail.com>
2018-02-11 11:26:49 -0500

    new spending

--- spending.php
+++ spending.php
@@ -15,27 +15,7 @@
 
 			<div class="spending">
 				<form action="insert_transaction.php" method="post">
-
-		<label for="">Date:</label>
-		<input type="date" id="datet" name="datet">
-		<br>
-		<label for="amount">Amount:</label>
-		<input type="number" step=".01" id="amount" name="amount" placeholder="Enter the Amount">
-		<br>
-		<label for="account">Account:</label>
-		<select id="account" name="account">
-			<option value="Chase">Chase</option>
-			<option value="Bank of America">Bank of America</option>
-			<option value="TD">TD</option>
-			<option value="Citi Bank">Citi Bank</option>
-		</select>
-		
-		<label for="catgory">Category:</label>
-		<select id="category" name="category">
-			<option value="grocery">Grocery</option>
-			<option value="bills">Bills</option>
-			<option value="entertainment">Entertainment</option>
-			<option value="eating out">Eating Out</option>
+Eating Out</option>
 		</select>
 
 			<label for="description">Description:</label>
